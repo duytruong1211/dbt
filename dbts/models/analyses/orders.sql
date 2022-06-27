ac074192af986f13335eca81533f11b3cca85b7d
Duy Truong <duytruong@Duys-MBP.attlocal.net>
2022-06-27 14:14:17 -0500

    update pmt table

--- dbts/models/analyses/orders.sql
+++ dbts/models/analyses/orders.sql
@@ -16,13 +16,28 @@ select
 from
 	brazilian_data.order_reviews r
 group by 1
+	),
+payments as (
+select
+	order_id,
+	max(payment_sequential) num_payment,
+	string_agg(distinct payment_type, ', ') pmt_type,
+	avg(payment_installments) avg_pmt_installment,
+	sum(payment_value) order_value
+from
+	brazilian_data.order_payments
+group by 1
 	)
 select
 	o.*,
+	p.num_payment,
+	p.pmt_type,
+	p.avg_pmt_installment,
 	r.avg_review_score
 from 
 	brazilian_data.orders o 
 	left join reviews r on r.order_id = o.order_id
+	left join payments p on p.order_id  = o.order_id
 /*
     Uncomment the line below to remove records with null `id` values
 */
