da463b72f09034b4b6b8dfbebbb165756596a883
Duy Truong <duytruong@Duys-MBP.attlocal.net>
2022-06-28 23:26:52 -0500

    update

--- dbts/models/analyses/customers.sql
+++ dbts/models/analyses/customers.sql
@@ -1,7 +1,10 @@
-
 -- Use the `ref` function to select from other models
-
-select *
-from {{ ref('orders') }}
-limit 10
+{{config(materialized='table')}}
+select 
+	ui.*,
+	g.geolocation_lat ,
+	g.geolocation_lng 
+from
+	brazilian_data.user_info ui 
+	inner join brazilian_data.geolocation g on g.geolocation_zip_code_prefix = ui.customer_zip_code_prefix 
 -- where order_id = 1
