1077412d501cafdb944bb16a18622d4101d08700
LongAtGitHub <98071520+LongAtGitHub@users.noreply.github.com>
2022-07-04 14:34:08 +0700

    Change customer big table

--- dbts/models/analyses/customers.sql
+++ dbts/models/analyses/customers.sql
@@ -1,14 +1,15 @@
 -- Use the `ref` function to select from other models
 {{config(materialized='table')}}
-with geolocation as (
-    select distinct*
-    from brazilian_data.geolocation
-)
-select 
-	ui.*,
-	g.geolocation_lat ,
-	g.geolocation_lng 
-from
-	brazilian_data.user_info ui 
-	inner join geolocation g on g.geolocation_zip_code_prefix = ui.customer_zip_code_prefix 
+select* from brazilian_data.user_info
+-- with geolocation as (
+--     select distinct*
+--     from brazilian_data.geolocation
+-- )
+-- select 
+-- 	ui.*,
+-- 	g.geolocation_lat ,
+-- 	g.geolocation_lng 
+-- from
+-- 	brazilian_data.user_info ui 
+-- 	inner join geolocation g on g.geolocation_zip_code_prefix = ui.customer_zip_code_prefix 
 -- where order_id = 1
