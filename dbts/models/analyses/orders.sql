1c3e0a2df407b22635ad348d95d117a719158f44
LongAtGitHub <98071520+LongAtGitHub@users.noreply.github.com>
2022-07-04 14:51:12 +0700

    Change to order.sql and remove customer.sql

--- dbts/models/analyses/orders.sql
+++ dbts/models/analyses/orders.sql
@@ -38,21 +38,33 @@ bad_data as (
 select order_id  from brazilian_data.orders o
 where  (o.order_delivered_customer_date::date < o.order_approved_at::date) or
  (order_status = 'delivered' and order_delivered_customer_date is null)
-)
+),
+result as (
 select
-	o.*,
+	o.order_id ,
+	o.order_status,
+	o.order_approved_at,
+	o.order_delivered_carrier_date,
+	o.order_delivered_customer_date,
+	o.order_estimated_delivery_date,
 	p.num_payment,
 	p.pmt_type,
 	p.avg_pmt_installment,
 	r.avg_review_score,
-	i.num_items
+	i.num_items,
+	ui.customer_unique_id,
+	ui.customer_city,
+	ui.customer_state 
 from 
 	brazilian_data.orders o 
 	left join bad_data on bad_data.order_id = o.order_id 
 	left join reviews r on r.order_id = o.order_id
 	left join payments p on p.order_id  = o.order_id
 	left join items i on i.order_id = o.order_id
-where bad_data.order_id is null 
+	inner join brazilian_data.user_info ui on ui.customer_id = o.customer_id 
+where bad_data.order_id is null
+)
+select* from result
 
 
 
