87489a8104101a53a6a8f58edc5d992dafae11fa
LongAtGitHub <98071520+LongAtGitHub@users.noreply.github.com>
2022-07-07 22:21:31 +0700

    Re-add order purchase timestamp

--- dbts/models/analyses/orders.sql
+++ dbts/models/analyses/orders.sql
@@ -34,6 +34,7 @@ select
 	o.order_id ,
 	o.order_status,
 	o.order_approved_at,
+	o.order_purchase_timestamp,
 	o.order_delivered_carrier_date,
 	o.order_delivered_customer_date,
 	o.order_estimated_delivery_date,
